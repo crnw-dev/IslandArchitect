186a076c779fd90d5c42b3678b6b4ed0a5038855
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2021-01-17 02:37:11 +0800

    Added the plugin main class
    - Added config and allow user to disable this plugin if they only want API (Lol for myself)

---
+++ src/Clouria/IslandArchitect/IslandArchitect.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+		
+		  _____     _                 _          
+		  \_   \___| | __ _ _ __   __| |         
+		   / /\/ __| |/ _` | '_ \ / _` |         
+		/\/ /_ \__ \ | (_| | | | | (_| |         
+		\____/ |___/_|\__,_|_| |_|\__,_|         
+		                                         
+		   _            _     _ _            _   
+		  /_\  _ __ ___| |__ (_) |_ ___  ___| |_ 
+		 //_\\| '__/ __| '_ \| | __/ _ \/ __| __|
+		/  _  \ | | (__| | | | | ||  __/ (__| |_ 
+		\_/ \_/_|  \___|_| |_|_|\__\___|\___|\__|
+		                                         
+														*/
+
+declare(strict_types=1);
+namespace Clouria\IslandArchitect;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\{
+	Listener
+}
+
+final class IslandArchitect extends PluginBase implements Listener {
+
+	private static $instance = null;
+
+	public function onLoad() : void {
+		self::$instance = $this;
+	}
+
+	public function onEnable() : void {
+		if (!$this->initConfig()) {
+			$this->getServer()->getPluginManager()->disablePlugin($this);
+			return;
+		}
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	private function initConfig() : bool {
+		$this->saveDefaultConfig();
+		$conf = $this->getConfig();
+		foreach ($all = $conf->getAll() as $k => $v) $conf->remove($k);
+
+		$conf->set('enable-plugin', (bool)($all['enable-plugin'] ?? true));
+		$conf->set('json-output', (bool)($all['json-output'] ?? $this->getDataFolder()));
+
+		return (bool)$conf->get('enable-plugin', true);
+	}
+
+	public static function getInstance() : ?self {
+		return self::$instance;
+	}
+	
+}
