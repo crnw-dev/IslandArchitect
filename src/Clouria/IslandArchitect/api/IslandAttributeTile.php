d2c355d96fa3adf54ffcb88fd51e419967d80af9
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2021-02-14 02:23:59 +0800

    Rewrote the island attribute class
    - Added island attribute types
    - Added world spawn attribute
    - Added chest position attribute

--- src/Clouria/IslandArchitect/api/IslandAttributeTile.php
+++ src/Clouria/IslandArchitect/api/IslandAttributeTile.php
@@ -31,22 +31,48 @@ use Clouria\IslandArchitect\api\RandomGeneration;
 
 class IslandAttributeTile extends Tile {
 
+	public const ATTRIB_RANDOM_GENERATION = 'random-generation';
+	public const ATTRIB_WORLD_SPAWN = 'world-spawn';
+	public const ATTRIB_ISLAND_CHEST = 'island-chest';
+
 	private $nbt = null;
 
-	public function getRandomGeneration() : RandomGeneration {
-		return RandomGeneration::fromNBT($this->nbt);
+	public function hasAttribute(string $attribute) : bool {
+		$tag = $nbt->getTag($attribute);
+		if ($tag === null) return false;
+		if ($tag instanceof ByteTag) return (bool)$tag->getValue();
+		return true;
+	}
+
+	public function setRandomGeneration(ListTag $regex) : void {
+		$regex = clone $regex;
+		$regex->setName('regex');
+		$nbt->setTag(new CompoundTag(self::ATTRIB_RANDOM_GENERATION, [$regex]));
+	}
+
+	public function setSpawnPosition() : void {
+		$this->nbt->setByte(self::ATTRIB_WORLD_SPAWN, 1);
+	}
+
+	public function setChestPosition() : void {
+		$this->nbt->setByte(self::ATTRIB_ISLAND_CHEST, 1);
+	}
+
+	public function getRandomGeneration() : ?RandomGeneration {
+		if (!$this->hasAttribute(self::ATTRIB_RANDOM_GENERATION)) return null;
+		return RandomGeneration::fromNBT($this->nbt->getTag(self::ATTRIB_RANDOM_GENERATION)->getListTag('regex'));
 	}
 
 	protected function readSaveData(CompoundTag $nbt) : void{
-		$this->nbt = clone $nbt->getListTag('regex');
+		$this->nbt = $nbt->getTag('IslandArchitect') ?? new CompoundTag('IslandArchitect');
 	}
 
 	protected function writeSaveData(CompoundTag $nbt) : void{
 		$nbt->setTag($this->nbt);
 	}
 
-	protected static function createAdditionalNBT(CompoundTag $nbt, Vector3 $pos, ?int $face = null, ?Item $item = null, ?Player $player = null) : void {
-		$nbt->setTag(clone $item->getNamedTagEntry('IslandArchitect')->getCompoundTag('random-generation')->getListTag('regex'));
+	protected function addAdditionalSpawnData(CompoundTag $nbt) : void {
+		$nbt->setTag(new CompoundTag('IslandArchitect', []));
 	}
 
 }
