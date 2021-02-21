34cbf0b7de5c32fc66c3a1b34b7f9c1a089e1723
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2021-02-21 22:31:13 +0800

    Fixed the island emit async task crashes if the island start or end coord is not set

--- src/Clouria/IslandArchitect/runtime/TemplateIsland.php
+++ src/Clouria/IslandArchitect/runtime/TemplateIsland.php
@@ -214,8 +214,10 @@ class TemplateIsland {
 
 	public function save() : string {
 		$data['level'] = $this->getLevel();
-		$data['startcoord'] = $this->getStartCoord()->floor();
-		$data['endcoord'] = $this->getEndCoord()->floor();
+		if (($vec = $this->getStartCoord()) !== null) $data['startcoord'] = $vec->getFloorX() . ':' . $vec->getFloorY() . ':' . $vec->getFloorZ();
+		else $data['startcoord'] = null;
+		if (($vec = $this->getEndCoord()) !== null) $data['endcoord'] = $vec->getFloorX() . ':' . $vec->getFloorY() . ':' . $vec->getFloorZ();
+		else $data['endcoord'] = null;
 		$data['random_blocks'] = $this->random_blocks;
 		foreach ($this->symbolic as $regexid => $symbolic) {
 			$symbolic = $symbolic[0] . (isset($symbolic[1]) ? ':' . $symbolic[1] : '');
