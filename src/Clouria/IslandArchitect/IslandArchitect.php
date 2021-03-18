58782fc264b39b47cbf748577444abc2c648d6dd
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2021-03-19 02:00:01 +0800

    Fixed the shape of template island start / end coord border particle (Brain burning mafs. The source of hollow cube drawing is from BuilderTools, https://github.com/CzechPMDevs/BuilderTools/blob/c9af6d93be19d69ef32aab853bc093202f2f0d44/src/czechpmdevs/buildertools/math/BlockGenerator.php#L57-L69)

--- src/Clouria/IslandArchitect/IslandArchitect.php
+++ src/Clouria/IslandArchitect/IslandArchitect.php
@@ -21,30 +21,27 @@ declare(strict_types=1);
 namespace Clouria\IslandArchitect;
 
 
-use Clouria\IslandArchitect\{
-    customized\skyblock\CustomSkyBlockCreateCommand,
-    runtime\sessions\PlayerSession,
-    runtime\TemplateIsland,
-    runtime\TemplateIslandGenerator};
-
-use muqsit\invmenu\InvMenuHandler;
 use room17\SkyBlock\SkyBlock;
-
+use muqsit\invmenu\InvMenuHandler;
+use Clouria\IslandArchitect\{
+    runtime\TemplateIsland,
+    runtime\sessions\PlayerSession,
+    runtime\TemplateIslandGenerator,
+    customized\skyblock\CustomSkyBlockCreateCommand};
 use pocketmine\{
-    level\format\Chunk,
-    level\generator\GeneratorManager,
-    level\Level,
-    level\particle\RedstoneParticle,
-    math\AxisAlignedBB,
-    math\Vector3,
     Player,
-    plugin\PluginBase,
-    scheduler\ClosureTask,
-    tile\Chest,
     tile\Tile,
+    tile\Chest,
+    level\Level,
+    utils\Utils,
+    math\Vector3,
+    plugin\PluginBase,
+    level\format\Chunk,
+    math\AxisAlignedBB,
+    scheduler\ClosureTask,
     utils\TextFormat as TF,
-    utils\Utils};
-
+    level\particle\RedstoneParticle,
+    level\generator\GeneratorManager};
 use function substr;
 use function strtolower;
 use function file_exists;
@@ -95,17 +92,44 @@ class IslandArchitect extends PluginBase {
                 if ($chest !== null and $chest->asVector3()->equals($sb->asVector3())) $s->getPlayer()->sendPopup(TF::YELLOW . 'Island chest block' . "\n" . TF::ITALIC . TF::GRAY . '(Click to view or edit contents)');
 
 		        // Draw island area outline
-                $bb = new AxisAlignedBB(
-                    min($sc->getFloorX(), $ec->getFloorX()),
-                    min($sc->getFloorY(), $ec->getFloorY()),
-                    min($sc->getFloorZ(), $ec->getFloorZ()),
-                    max($sc->getFloorX(), $ec->getFloorX()),
-                    max($sc->getFloorY(), $ec->getFloorY()),
-                    max($sc->getFloorZ(), $ec->getFloorZ())
-                );
-                $bb->offset(0.5, 0.5, 0.5);
-                $bb->expand(0.5, 0.5, 0.5);
-                for ($x=$bb->minX; $x <= $bb->maxX; $bb+=0.5) for ($y=$bb->minY; $y <= $bb->maxY; $bb+=0.5) for ($z=$bb->minZ; $z <= $bb->maxZ; $bb+=0.5) $s->getPlayer()->getLevel()->addParticle(new RedstoneParticle(new Vector3($x, $y, $z), 10), [$s->getPlayer()]);
+                if ($sc !== null and $ec !== null and $s->getPlayer()->getLevel()->getFolderName() === $s->getIsland
+                    ()->getLevel()) {
+                    $bb = new AxisAlignedBB(
+                        min($sc->getFloorX(), $ec->getFloorX()),
+                        min($sc->getFloorY(), $ec->getFloorY()),
+                        min($sc->getFloorZ(), $ec->getFloorZ()),
+                        max($sc->getFloorX(), $ec->getFloorX()),
+                        max($sc->getFloorY(), $ec->getFloorY()),
+                        max($sc->getFloorZ(), $ec->getFloorZ())
+                    );
+                    $bb->offset(0.5, 0.5, 0.5);
+                    $bb->expand(0.5, 0.5, 0.5);
+                    $distance = $s->getPlayer()->getViewDistance();
+                    $dbb = (new AxisAlignedBB(
+                    ($s->getPlayer()->getFloorX() >> 4) - $distance,
+                    0,
+                    ($s->getPlayer()->getFloorZ() >> 4) - $distance,
+                    ($s->getPlayer()->getFloorX() >> 4) + $distance,
+                    Level::Y_MAX,
+                    ($s->getPlayer()->getFloorZ() >> 4) + $distance
+                    ))->expand(1, 1, 1);
+                    for ($x = $bb->minX; $x <= $bb->maxX; ++$x)
+                    for ($y = $bb->minY; $y <= $bb->maxY; ++$y)
+                    for ($z = $bb->minZ; $z <= $bb->maxZ; ++$z)
+                    {
+                        if (!$dbb->isVectorInside(new Vector3((int)$x >> 4, (int)$y >> 4, (int)$z >> 4))) continue;
+                        if (
+                            $x !== $bb->minX and
+                            $y !== $bb->minY and
+                            $z !== $bb->minZ and
+                            $x !== $bb->maxX and
+                            $y !== $bb->maxY and
+                            $z !== $bb->maxZ
+                        ) continue;
+                        $s->getPlayer()->getLevel()->addParticle(new RedstoneParticle(new Vector3($x ,$y, $z), 10),
+                            [$s->getPlayer()]);
+                    }
+                }
             }
         }), 10);
 	}
