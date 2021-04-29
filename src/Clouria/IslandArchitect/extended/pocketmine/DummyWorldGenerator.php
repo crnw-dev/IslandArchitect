51ed61dcff14cff37e6d94a6741c1e8f36f16ed2
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2021-04-29 23:18:19 +0800

    Added dummy generator classes

---
+++ src/Clouria/IslandArchitect/extended/pocketmine/DummyWorldGenerator.php
@@ -0,0 +1,54 @@
+<?php
+/*
+		
+		  _____     _                 _          
+		  \_   \___| | __ _ _ __   __| |         
+		   / /\/ __| |/ _` | '_ \ / _` |         
+		/\/ /_ \__ \ | (_| | | | | (_| |
+		\____/ |___/_|\__,_|_| |_|\__,_|         
+		                                         
+		   _            _     _ _            _   
+		  /_\  _ __ ___| |__ (_) |_ ___  ___| |_ 
+		 //_\\| '__/ __| '_ \| | __/ _ \/ __| __|
+		/  _  \ | | (__| | | | | ||  __/ (__| |_ 
+		\_/ \_/_|  \___|_| |_|_|\__\___|\___|\__|
+		                                         
+		@ClouriaNetwork | Apache License 2.0
+		
+        ██╗  ██╗    ██╗  ██╗    
+        ██║  ██║    ██║ ██╔╝    光   時   LIBERATE   
+        ███████║    █████╔╝     復   代   HONG
+        ██╔══██║    ██╔═██╗     香   革   KONG
+        ██║  ██║    ██║  ██╗    港   命   
+        ╚═╝  ╚═╝    ╚═╝  ╚═╝
+                    
+														*/
+declare(strict_types=1);
+
+namespace Clouria\IslandArchitect\extended\pocketmine;
+
+use pocketmine\math\Vector3;
+use pocketmine\level\generator\Generator;
+
+class DummyWorldGenerator extends Generator {
+
+    public function __construct(array $settings = []) { }
+
+    public function generateChunk(int $chunkX, int $chunkZ) : void {
+        $this->level->getChunk($chunkX, $chunkZ)->setGenerated(true);
+    }
+
+    public function populateChunk(int $chunkX, int $chunkZ) : void { }
+
+    public function getSettings() : array {
+        return [];
+    }
+
+    public function getName() : string {
+        return 'islandarchitect-generator';
+    }
+
+    public function getSpawn() : Vector3 {
+        return new Vector3(0, 0, 0);
+    }
+}
