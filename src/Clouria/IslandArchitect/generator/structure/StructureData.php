8d813450d85f9ff4dc8a996cba55a2241461a70e
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2021-07-11 18:56:05 +0800

    Revert "Replaced the chunk map with chunk data length"
    This reverts commit cd87ffeb

--- src/Clouria/IslandArchitect/generator/structure/StructureData.php
+++ src/Clouria/IslandArchitect/generator/structure/StructureData.php
@@ -27,16 +27,10 @@ declare(strict_types=1);
 
 namespace Clouria\IslandArchitect\generator\structure;
 
-use pocketmine\level\Level;
 use pocketmine\utils\Binary;
 use Clouria\IslandArchitect\Utils;
-use function abs;
 use function fseek;
-use function substr;
-use function strlen;
 use function is_resource;
-use const SEEK_CUR;
-use const PHP_INT_MAX;
 
 class StructureData {
 
@@ -58,20 +52,23 @@ class StructureData {
     public function decode() {
         if (!is_resource($this->stream)) return;
         fseek($this->stream, 0);
-        $header = Utils::ReadAndSeek($this->stream, 10);
-        // Format version (1), sub version (1), signed length of first chunk (8)
+        $header = Utils::ReadAndSeek($this->stream, 3);
+        // Format version (1), sub version (1), chunk hash size (1)
 
         $ver = Binary::readByte($header[0]);
         if ($ver > self::FORMAT_VERSION) $this->panicParse("Unsupported structure format version " . $ver, false);
 
-        $clength = substr($header, 2);
-        if (strlen($clength) !== 8) return; // Empty structure
-        $clength = Binary::readLLong($clength);
+        $chashsize = [
+                         0 => 1,
+                         1 => 2,
+                         3 => 4,
+                         4 => 8,
+                     ][$header[3]];
+        $chunkmap = Utils::ReadAndSeek($this->stream, $chashsize * 8);
+        // An array of chunk hash followed by file offset (8), max limit 1MB per chunk
 
-        Level::getXZ($this->chunkhash, $cx, $cz);
-        for ($x = 0; $x <= $cx; $x++) for ($z = 0; $z <= $cz; $z++) {
-            if ($clength < 0) fseek($this->stream, PHP_INT_MAX, SEEK_CUR); // Max limit 2MB per chunk
-            fseek($this->stream, (int)(abs($clength) - ($clength < 0 ? 1 : 0)), SEEK_CUR);
+        for ($cpointer = 0; $cpointer < $chunkmap / ($chashsize + 8); $chunkmap += ($chashsize + 8)) {
+            // TODO: Read data with correct range by its length
         }
     }
 
