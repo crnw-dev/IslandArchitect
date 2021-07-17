02c52c7f8d10d03084ee75d7865f5804485a638d
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2021-07-17 19:36:42 +0800

    Fixed throwing exception at wrong place

--- src/Clouria/IslandArchitect/generator/structure/StructureData.php
+++ src/Clouria/IslandArchitect/generator/structure/StructureData.php
@@ -104,7 +104,11 @@ class StructureData {
                 case 3:
                     $pointer = ftell($this->stream);
                     if ($pointer === false) throw new \RuntimeException("Cannot fetch the position of pointer from file descriptor");
-                    $this->loadProperty($bkraw % self::BKV_TYPE_SIZE)->run($this, $pointer, new Vector3($clocator % 16, $clocator >> 4 >> 4 % self::Y_MAX, ($clocator >> 4) % 16));
+                    $id = $bkraw % self::BKV_TYPE_SIZE;
+                    $prop = $this->loadProperty($id);
+                    if ($prop === null) throw new \RuntimeException('Structure property ' . $id . 'is required but cannot be found in the structure data');
+                    fseek($this->stream, $pointer);
+                    $prop->run($this, $pointer, new Vector3($clocator % 16, $clocator >> 4 >> 4 % self::Y_MAX, ($clocator >> 4) % 16));
                     break;
 
                 // Type 4 will not be handled as they are not used in this version of IslandArchitect
@@ -136,10 +140,9 @@ class StructureData {
 
     /**
      * @throws StructureParseException Property count cannot be loaded
-     * @throws \RuntimeException Property not found
      */
     public function loadProperty(int $id) : ?StructureProperty {
-        if ($this->getPropertiesCount() <= $id) throw new \RuntimeException('Structure property ' . $id . 'is required but cannot be found in the structure data');
+        if ($this->getPropertiesCount() <= $id) return null;
         fseek($this->stream, 2);
         for ($pointer = 0; $pointer < $id; $pointer++) fseek($this->stream, Binary::readLShort(Utils::readAndSeek($this->stream, 2)), SEEK_CUR);
 
