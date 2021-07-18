a356407827de7ef0606a7e6001c2475388394c84
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2021-07-19 00:16:17 +0800

    Added the StructureAttachment interface

---
+++ src/Clouria/IslandArchitect/generator/attachments/StructureAttachment.php
@@ -0,0 +1,44 @@
+<?php
+/*
+		
+		  _____     _                 _          
+		  \_   \___| | __ _ _ __   __| |         
+		   / /\/ __| |/ _` | '_ \ / _` |         
+		/\/ /_ \__ \ | (_| | | | | (_| |
+		\____/ |___/_|\__,_|_| |_|\__,_|         
+		                                         
+		   _            _     _ _            _   
+		  /_\  _ __ ___| |__ (_) |_ ___  ___| |_ 
+		 //_\\| '__/ __| '_ \| | __/ _ \/ __| __|
+		/  _  \ | | (__| | | | | ||  __/ (__| |_ 
+		\_/ \_/_|  \___|_| |_|_|\__\___|\___|\__|
+		                                         
+		@ClouriaNetwork | Apache License 2.0
+		
+        ██╗  ██╗    ██╗  ██╗    
+        ██║  ██║    ██║ ██╔╝    光   時   LIBERATE   
+        ███████║    █████╔╝     復   代   HONG
+        ██╔══██║    ██╔═██╗     香   革   KONG
+        ██║  ██║    ██║  ██╗    港   命   
+        ╚═╝  ╚═╝    ╚═╝  ╚═╝
+                    
+														*/
+declare(strict_types=1);
+
+namespace Clouria\IslandArchitect\generator\attachments;
+
+use pocketmine\math\Vector3;
+use Clouria\IslandArchitect\generator\structure\StructureData;
+
+interface StructureAttachment {
+
+    /**
+     * @param StructureData $data
+     * @param int $pointer Pointer of the stream (file descriptor / websocket...) in StructureData
+     * @param Vector3 $pos Current block position in chunk (0-15, 0-255, 0-15)
+     * @param int $repeat Count of remaining repeat cycles
+     * @return mixed
+     */
+    public function run(StructureData $data, int &$pointer, Vector3 $pos, int $repeat);
+
+}
